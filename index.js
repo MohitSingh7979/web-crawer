6b91e8190996af8e851a342a9f17758fad59a2b4
MohitSingh7979 <mohitsinghatwork@gmail.com>
2024-09-16 19:14:57 +0530

    Remove: node module and package lock file

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import { JSDOM } from "jsdom";
 
-setInterval(()=>{
+setInterval(() => {
     const data = await fetch("https://deployment.setchartron.in/Home.aspx");
-const html = await data.text();
-// console.log(html);
+    const html = await data.text();
+    // console.log(html);
 
-const dom = new JSDOM(html);
-const document = dom.window.document;
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
 
-const postNotification = document.querySelector("[id*=ContentPlaceHolder1_Panel]");
+    const postNotification = document.querySelector("[id*=ContentPlaceHolder1_Panel]");
 
-const links = postNotification.querySelectorAll("a");
-links.forEach(link => {
-    console.log(link.href);
-    console.log(link.textContent);
-});
-},1000*60*60*6)
+    const links = postNotification.querySelectorAll("a");
+    links.forEach(link => {
+        console.log(link.href);
+        console.log(link.textContent);
+    });
+}, 1000 * 60 * 60 * 6)
